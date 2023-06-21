a009f5e5e37b4032c04a9abf4a60fed6c54b2d72
unknown <admob5364@gmail.com>
2023-06-22 01:40:58 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const noteRouter = require("./routes/noteRoutes");
+const userRouter = require("./routes/userRoutes");
+const mongoose= require("mongoose");
+const dotenv=require("dotenv");
+const cors =require("cors");
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRouter);
+app.use("/note", noteRouter);
+app.get("/", (req, res) =>{
+res.send("NotesApi");
+});
+
+const port = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGOURL)
+.then(()=>{
+app.listen (port, ()=>{
+console.log("Server started on port no. "+port);
+});
+
+})
+.catch((error) => {
+console.log(error);
+})
+
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const { signup, signin } = require("../controllers/userController");
+const userRouter= express. Router();
+userRouter.post("/signup", signup);
+userRouter.post("/signin", signin);
+module.exports = userRouter;
